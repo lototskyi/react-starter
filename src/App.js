b577c48db32acdbe034603e5566c8c5380d37485
alex <o.lototskyi@gmail.com>
2022-04-20 12:02:54 +0300

    Transition

--- src/App.js
+++ src/App.js
@@ -1,40 +1,70 @@
 import {useState} from 'react';
 import {Container} from 'react-bootstrap';
+import { Transition } from 'react-transition-group';
 import './App.css';
 
 const Modal = (props) => {
 
+    const duration = 300;
+
+    const defaultStyle = {
+        transition: `all ${duration}ms ease-in-out`,
+        opacity: 0,
+        visibility: 'hidden'
+    }
+
+    const transitionStyles = {
+        entering: { opacity: 1, visibility: 'visible' },
+        entered:  { opacity: 1, visibility: 'visible' },
+        exiting:  { opacity: 0, visibility: 'hidden' },
+        exited:  { opacity: 0, visibility: 'hidden' },
+    };
+
     return (
-        <div className="modal mt-5 d-block">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Typical modal window</h5>
-                    <button onClick={() => props.onClose(false)} type="button" className="btn-close" aria-label="Close"></button>
+        <Transition 
+            in={props.show} 
+            timeout={duration} 
+            onEnter={() => props.setShowTrigger(false)}
+            onExited={() => props.setShowTrigger(true)}>
+            {state => (
+                <div className="modal mt-5 d-block" style={{
+                    ...defaultStyle,
+                    ...transitionStyles[state]
+                  }}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Typical modal window</h5>
+                            <button onClick={() => props.onClose(false)} type="button" className="btn-close" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Modal body content</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
+                            <button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="modal-body">
-                    <p>Modal body content</p>
-                </div>
-                <div className="modal-footer">
-                    <button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
-                    <button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
-                </div>
-                </div>
-            </div>
-        </div>
+            )}
+        </Transition>
     )
 }
 
 function App() {
     const [showModal, setShowModal] = useState(false);
+    const [showTrigger, setShowTrigger] = useState(true);
 
     return (
         <Container>
-            {showModal ? <Modal onClose={setShowModal}/> : null}
+            <Modal show={showModal} onClose={setShowModal} setShowTrigger={setShowTrigger}/>
+            {showTrigger ? 
             <button 
                 type="button" 
                 className="btn btn-warning mt-5"
-                onClick={() => setShowModal(true)}>Open Modal</button>
+                onClick={() => setShowModal(true)}>Open Modal</button> :
+            null}
         </Container>
     );
 }
